918a3e227d226688ff17bb963e19e81be8f81612
kris <k.frey@acfr.usyd.edu.au>
2014-08-09 00:12:15 +1000

    Renamed build_lib.launch to build_lib_weeding.launch. Defined target weeding volume in build_lib_weeding.cpp

--- src/build_lib_weeding.cpp
+++ src/build_lib_weeding.cpp
@@ -18,6 +18,34 @@ int main(int argc, char** argv)
     tlib.addSphereCollisionObject(BUSH_RADIUS);
     tlib.printCollisionWorldInfo(std::cout);
 
+    ROS_INFO("Press Enter to begin generating joint values.");
+    std::cin.ignore(100, '\n');
+
+    // Define weed soil volume grid
+    target_volume weedSoilVol;
+    weedSoilVol.type = GRID_RECT;
+    weedSoilVol.grid.orientation.w = sqrt(0.5);
+    weedSoilVol.grid.orientation.x = 0;
+    weedSoilVol.grid.orientation.y = -sqrt(0.5);
+    weedSoilVol.grid.orientation.z = 0;
+    weedSoilVol.grid.xlim_low = -.35;
+    weedSoilVol.grid.xlim_high = 0.35;
+    weedSoilVol.grid.xres = 3;
+    weedSoilVol.grid.ylim_low = -.35;
+    weedSoilVol.grid.ylim_high = 0.35;
+    weedSoilVol.grid.yres = 3;
+    weedSoilVol.grid.zlim_low = 0.80;
+    weedSoilVol.grid.zlim_high = 0.95;
+    weedSoilVol.grid.zres = 1;
+    weedSoilVol.allow_internal_paths = true;
+
+    std::vector<target_volume> t_vols;
+    t_vols.push_back(weedSoilVol);
+
+    /* Generate target joint values */
+    ROS_INFO("Calculating target joint values.");
+    tlib.generateTargets(t_vols);
+
     ROS_INFO("Hit enter to begin building library.");
     std::cin.ignore(100, '\n');
 
